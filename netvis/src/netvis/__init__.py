e37bf56716fbf51babada6fcde02a51bd75fa8fb
Gautam Singh <goats.singh@gmail.com>
2023-08-22 06:51:39 +0530

    Use raw url to get tsv file

--- netvis/src/netvis/__init__.py
+++ netvis/src/netvis/__init__.py
@@ -1,3 +1,3 @@
 # Load the data in the dataframe
 import pandas as pd
-asn_df = pd.read_csv('data/asn.tsv', sep='\t', header=0, index_col=None, usecols=['Asn', 'As_name']).drop_duplicates(keep='first')
+asn_df = pd.read_csv('https://raw.githubusercontent.com/goats-9/cs3530-assignments/main/netvis/data/asn.tsv', sep='\t', header=0, index_col=None, usecols=['Asn', 'As_name']).drop_duplicates(keep='first')
