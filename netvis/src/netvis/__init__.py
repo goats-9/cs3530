227508df3e9fd966bbfaf15a6320f29744157e89
Gautam Singh <goats.singh@gmail.com>
2023-08-23 07:36:41 +0530

    Remove `load.py`

--- netvis/src/netvis/__init__.py
+++ netvis/src/netvis/__init__.py
@@ -4,4 +4,3 @@
 import pandas as pd
 asn_df = pd.read_csv('https://raw.githubusercontent.com/goats-9/cs3530-assignments/main/netvis/data/asn.tsv', sep='\t', header=0, index_col=None, usecols=['Asn', 'As_name']).drop_duplicates(keep='first')
 from . import NetGraph
-from . import load
--- netvis/src/netvis/load.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from . import NetGraph
-
-def load(filename):
-    node_df = pd.read_excel(filename, sheet_name='nodes')
-    edge_df = pd.read_excel(filename, sheet_name='edges')
-    return NetGraph.NetGraph(nodes=node_df, edge_list=edge_df)
