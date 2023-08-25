8cd4d5ab45ce76e5ba341ca6afba937295e02925
Gautam Singh <goats.singh@gmail.com>
2023-08-25 18:21:09 +0530

    Add ips of destinations

---
+++ netvis/tests/dest_gen.py
@@ -0,0 +1,9 @@
+import socket
+
+fh = open('data/urls.txt', 'r')
+L = fh.readlines()
+fh.close()
+with open('data/ips.txt', 'w') as fh:
+    for line in L:
+        ip_addr = socket.gethostbyname(line.rstrip())
+        fh.write(ip_addr+'\n')
